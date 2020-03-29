7ed075a5c78eb74b0d8eaac287e2ee787d79d28d
jakabij <jakabistvanj@gmail.com>
2020-03-29 17:41:05 +0200

    Third task completed.

--- northwind.sql
+++ northwind.sql
@@ -1,5 +1,7 @@
-Select Suppliers.company_name AS Company, COUNT(Suppliers.supplier_id) AS Products FROM Suppliers
+
+SELECT Company from (Select Suppliers.company_name AS Company, COUNT(Suppliers.supplier_id) AS Products FROM Suppliers
 INNER JOIN Products
 ON Products.supplier_id=Suppliers.supplier_id
 GROUP BY Suppliers.supplier_id
-ORDER BY COUNT(Suppliers.supplier_id) DESC, Suppliers.company_name;
+HAVING COUNT(Suppliers.supplier_id)=5
+ORDER BY COUNT(Suppliers.supplier_id) DESC, Suppliers.company_name) AS Company;
