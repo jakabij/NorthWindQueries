7b71aba7093c4510a62b97041f424299d758203b
jakabij <jakabistvanj@gmail.com>
2020-03-29 14:56:20 +0200

    Second part completed

--- northwind.sql
+++ northwind.sql
@@ -1,4 +1,5 @@
-Select product_name AS Product, company_name AS Company from Suppliers
+Select Suppliers.company_name AS Company, COUNT(Suppliers.supplier_id) AS Products FROM Suppliers
 INNER JOIN Products
 ON Products.supplier_id=Suppliers.supplier_id
-ORDER BY product_name, company_name;
+GROUP BY Suppliers.supplier_id
+ORDER BY COUNT(Suppliers.supplier_id) DESC, Suppliers.company_name;
