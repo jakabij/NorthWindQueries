16d4ba72a55e64b5ad301f209ad274195c0233f7
jakabij <jakabistvanj@gmail.com>
2020-03-30 09:10:11 +0200

    Last task completed

--- northwind.sql
+++ northwind.sql
@@ -1,7 +1,6 @@
-
-SELECT Company from (Select Suppliers.company_name AS Company, COUNT(Suppliers.supplier_id) AS Products FROM Suppliers
+SELECT Suppliers.company_name as Company, Products.product_name as Product,
+		MAX(Products.unit_price) as Price FROM Suppliers
 INNER JOIN Products
-ON Products.supplier_id=Suppliers.supplier_id
-GROUP BY Suppliers.supplier_id
-HAVING COUNT(Suppliers.supplier_id)=5
-ORDER BY COUNT(Suppliers.supplier_id) DESC, Suppliers.company_name) AS Company;
+ON Suppliers.supplier_id=Products.supplier_id
+GROUP BY Suppliers.company_name,Products.product_name
+ORDER BY Price DESC;
